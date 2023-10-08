1c33b65799d506a9d9bb4306139706aa68fea737
mon-jai <91261297+mon-jai@users.noreply.github.com>
2023-10-08 23:26:22 +0800

    Add an option to use the Windows title bar (#912)
    Co-authored-by: Brendan Forster <github@brendanforster.com>

--- app/src/lib/stores/app-store.ts
+++ app/src/lib/stores/app-store.ts
@@ -80,6 +80,7 @@ import {
   getPersistedThemeName,
   setPersistedTheme,
 } from '../../ui/lib/application-theme'
+import { TitleBarStyle } from '../../ui/lib/title-bar-style'
 import {
   getAppMenu,
   getCurrentWindowState,
@@ -91,6 +92,8 @@ import {
   sendWillQuitEvenIfUpdatingSync,
   quitApp,
   sendCancelQuittingSync,
+  saveTitleBarStyle,
+  getTitleBarStyle,
 } from '../../ui/main-process-proxy'
 import {
   API,
@@ -546,6 +549,7 @@ export class AppStore extends TypedBaseStore<IAppState> {
   private selectedTheme = ApplicationTheme.System
   private currentTheme: ApplicableTheme = ApplicationTheme.Light
   private selectedTabSize = tabSizeDefault
+  private titleBarStyle: TitleBarStyle = 'native'
 
   private useWindowsOpenSSH: boolean = false
 
@@ -1052,6 +1056,7 @@ export class AppStore extends TypedBaseStore<IAppState> {
       selectedTheme: this.selectedTheme,
       currentTheme: this.currentTheme,
       selectedTabSize: this.selectedTabSize,
+      titleBarStyle: this.titleBarStyle,
       apiRepositories: this.apiRepositoriesStore.getState(),
       useWindowsOpenSSH: this.useWindowsOpenSSH,
       showCommitLengthWarning: this.showCommitLengthWarning,
@@ -2250,6 +2255,8 @@ export class AppStore extends TypedBaseStore<IAppState> {
       this.currentTheme = theme
       this.emitUpdate()
     })
+
+    this.titleBarStyle = await getTitleBarStyle()
 
     this.lastThankYou = getObject<ILastThankYou>(lastThankYouKey)
 
@@ -6597,6 +6604,14 @@ export class AppStore extends TypedBaseStore<IAppState> {
     }
 
     return Promise.resolve()
+  }
+
+  /*
+   * Set the title bar style for the application
+   */
+  public _setTitleBarStyle(titleBarStyle: TitleBarStyle) {
+    this.titleBarStyle = titleBarStyle
+    return saveTitleBarStyle(titleBarStyle)
   }
 
   public async _resolveCurrentEditor() {
