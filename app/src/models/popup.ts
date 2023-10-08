4fd8bd7238ab41031086a8f723377b15e7696f63
mon-jai <91261297+mon-jai@users.noreply.github.com>
2023-10-08 23:26:22 +0800

    Add an option to use the Windows title bar (#912)
    Co-authored-by: Brendan Forster <github@brendanforster.com>

--- app/src/models/popup.ts
+++ app/src/models/popup.ts
@@ -96,6 +96,7 @@ export enum PopupType {
   UnknownAuthors = 'UnknownAuthors',
   ConfirmRepoRulesBypass = 'ConfirmRepoRulesBypass',
   TestIcons = 'TestIcons',
+  ConfirmRestart = 'ConfirmRestart',
 }
 
 interface IBasePopup {
@@ -425,5 +426,6 @@ export type PopupDetail =
   | {
       type: PopupType.TestIcons
     }
+  | { type: PopupType.ConfirmRestart }
 
 export type Popup = IBasePopup & PopupDetail
