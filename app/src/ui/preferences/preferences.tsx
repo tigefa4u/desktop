171bf429d089b27292052031b992bed3f4cce394
mon-jai <91261297+mon-jai@users.noreply.github.com>
2023-10-08 23:26:22 +0800

    fix: add an option to use the Windows title bar (#912)
    Co-authored-by: Brendan Forster <github@brendanforster.com>

--- app/src/ui/preferences/preferences.tsx
+++ app/src/ui/preferences/preferences.tsx
@@ -22,6 +22,7 @@ import {
 } from '../lib/identifier-rules'
 import { Appearance } from './appearance'
 import { ApplicationTheme } from '../lib/application-theme'
+import { TitleBarStyle } from '../lib/title-bar-style'
 import { OkCancelButtonGroup } from '../dialog/ok-cancel-button-group'
 import { Integrations } from './integrations'
 import {
@@ -81,6 +82,7 @@ interface IPreferencesProps {
   readonly customEditor: ICustomIntegration | null
   readonly useCustomShell: boolean
   readonly customShell: ICustomIntegration | null
+  readonly titleBarStyle: TitleBarStyle
   readonly repositoryIndicatorsEnabled: boolean
   readonly onOpenFileInExternalEditor: (path: string) => void
   readonly underlineLinks: boolean
@@ -117,7 +119,7 @@ interface IPreferencesState {
   readonly selectedExternalEditor: string | null
   readonly availableShells: ReadonlyArray<Shell>
   readonly selectedShell: Shell
-
+  readonly titleBarStyle: TitleBarStyle
   /**
    * If unable to save Git configuration values (name, email)
    * due to an existing configuration lock file this property
@@ -187,6 +189,7 @@ export class Preferences extends React.Component<
       selectedExternalEditor: this.props.selectedExternalEditor,
       availableShells: [],
       selectedShell: this.props.selectedShell,
+      titleBarStyle: this.props.titleBarStyle,
       repositoryIndicatorsEnabled: this.props.repositoryIndicatorsEnabled,
       initiallySelectedTheme: this.props.selectedTheme,
       initiallySelectedTabSize: this.props.selectedTabSize,
@@ -467,6 +470,8 @@ export class Preferences extends React.Component<
             onSelectedThemeChanged={this.onSelectedThemeChanged}
             selectedTabSize={this.props.selectedTabSize}
             onSelectedTabSizeChanged={this.onSelectedTabSizeChanged}
+            titleBarStyle={this.props.titleBarStyle}
+            onTitleBarStyleChanged={this.onTitleBarStyleChanged}
           />
         )
         break
@@ -684,6 +689,10 @@ export class Preferences extends React.Component<
     this.props.dispatcher.setSelectedTabSize(tabSize)
   }
 
+  private onTitleBarStyleChanged = (titleBarStyle: TitleBarStyle) => {
+    this.setState({ titleBarStyle })
+  }
+
   private renderFooter() {
     const hasDisabledError = this.state.disallowedCharactersMessage != null
 
@@ -810,7 +819,9 @@ export class Preferences extends React.Component<
     if (this.state.selectedExternalEditor) {
       await dispatcher.setExternalEditor(this.state.selectedExternalEditor)
     }
+
     await dispatcher.setShell(this.state.selectedShell)
+    await dispatcher.setTitleBarStyle(this.state.titleBarStyle)
     await dispatcher.setConfirmDiscardChangesSetting(
       this.state.confirmDiscardChanges
     )
